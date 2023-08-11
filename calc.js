8da10ba46cce6d06fda59b52f8c230357e0601f4
Ashutosh Ranjan <85917906+ashutoshprasad94309@users.noreply.github.com>
2023-08-11 16:02:21 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,15 @@
+let userInput= document.getElementById("input")
+let expression=""
+
+function btn(num){
+    expression=expression+num
+    userInput.value=expression
+}
+function equal(){
+    userInput.value=eval(expression)
+    expression=userInput.value
+}
+function erase(){
+    expression=""
+    userInput.value=expression
+}
